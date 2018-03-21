9bf0906586bafb731182dc3e6569a5ab30e27643
youminxiaodao <29645485+youminxiaodao@users.noreply.github.com>
2018-03-21 22:47:53 +0800

    Update UseHostAndPortToConnect.py

--- UseHostAndPortToConnect.py
+++ UseHostAndPortToConnect.py
@@ -1,9 +1,9 @@
-import parse
+import optparse
 import socket
 from socket import *
 def connScan(tgtHost,tgtPort):
 	try:
-		connSkt = socket(AF_INET,SOCKET_STREAM)
+		connSkt = socket(AF_INET,SOCK_STREAM)
 		connSkt.connect((tgtHost,tgtPort))
 		connSkt.send('ViolentPython\r\n')
 		results = connSkt.recv(100)
@@ -13,14 +13,14 @@ def connScan(tgtHost,tgtPort):
 	except:
 		print '[-]%d/tcp closed'% tgtPort
 
-def portScan(tgtHost,tgtPort):
+def portScan(tgtHost,tgtPorts):
 	try:
 		tgtIp=gethostbyname(tgtHost)
 	except:
 		print "[-] Cannot resolve '%s': Unknown host"%tgtHost
 		return
 	try:
-		tgtName=gethostbyadddr(tgtIp)
+		tgtName=gethostbyaddr(tgtIp)
 		print '\n[+] Scan Results for: '+tgtName[0]
 	except:
 		print '\n[+] Scan Results for: '+tgtIp
@@ -31,20 +31,19 @@ def portScan(tgtHost,tgtPort):
 
 
 def main():
-	parser = optparse.OptionParser('usage %prog -H'+\
-	'<target host> -p <target port>')
-	print('parser:')
-	print (parser)
+	parser = optparse.OptionParser('usage%prog ' +\
+	'-H <target host> -p <target port>')
 	parser.add_option('-H',dest='tgtHost',type='string',\
 	help='specify target host')
-	parser.add_option('-p',dest='tgtPort',type='int',\
-	help='specify target port')
+	parser.add_option('-p',dest='tgtPort',type='string',\
+	help='specify target ports port[s] seperated by comma')
 	(options,args) = parser.parse_args()
 	tgtHost = options.tgtHost
 	tgtPorts = str(options.tgtPort).split(',')
-	if (tgtHost == None) |(tgtPort == None):
+	if (tgtHost == None) |(tgtPorts == None):
 		print '[-] You must specify a target host and port[s].' 
 		exit(0)
 	portScan(tgtHost,tgtPorts)
 	if __name__ == '__main__':
 		main()
+main()
